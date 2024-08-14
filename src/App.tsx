8251685f86dd78e8a7fb0a8e6c109e2ab12d796e
AyushKhamar <74979803+AyushKhamar@users.noreply.github.com>
2024-08-14 09:25:44 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <main>
+      <div className="flex b-2 p-2 "> hello there</div>
+      React ⚛️ + Vite ⚡ + Replit 🌀
+    </main>
+  );
+}
